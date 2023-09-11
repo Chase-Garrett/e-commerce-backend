c546be56843732a13dba50e5e5046c7af5c56a28
Chase <chasegarrett@tutanota.com>
2023-09-10 23:09:55 -0500

    add columns to category

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,16 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+
+    // category id
+    id: {
+      type: DataTypes.INTEGER,
+    },
+
+    // category name
+    category_name: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
