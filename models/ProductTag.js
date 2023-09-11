30f358d39981bcf41a7320ecbaad17a3388d6f99
Chase <chasegarrett@tutanota.com>
2023-09-10 23:14:33 -0500

    added columns to productTag

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,21 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+
+    // product tag id
+    id: {
+      type: DataTypes.INTEGER,
+    },
+
+    // product id
+    product_id: {
+      type: DataTypes.INTEGER,
+    },
+
+    // tag id
+    tag_id: {
+      type: DataTypes.INTEGER,
+    },
   },
   {
     sequelize,
